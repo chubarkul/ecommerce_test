dadd1f9c82d6b18613dfb5a200a684c11191ac36
chubarkul <58489131+chubarkul@users.noreply.github.com>
2023-06-20 17:45:21 +0600

    Update main.js

--- main.js
+++ main.js
@@ -81,39 +81,42 @@ function openPopup() {
   popup.style.display = "block"; // Show the pop-up
                 
   // dataLayer.push for purchase
-   dataLayer.push({"event", "purchase", {
-    transaction_id: "T_12345",
-    value: 25.42,
-    tax: 4.90,
-    shipping: 5.99,
-    currency: "USD",
-    coupon: "SUMMER_SALE",
-    items: [
-     {
-      item_id: "SKU_12345",
-      item_name: "Stan and Friends Tee",
-      affiliation: "Google Merchandise Store",
-      coupon: "SUMMER_FUN",
-      discount: 2.22,
-      index: 0,
-      item_brand: "Google",
-      item_category: "Apparel",
-      item_category2: "Adult",
-      item_category3: "Shirts",
-      item_category4: "Crew",
-      item_category5: "Short sleeve",
-      item_list_id: "related_products",
-      item_list_name: "Related Products",
-      item_variant: "green",
-      location_id: "ChIJIQBpAG2ahYAR_6128GcTUEo",
-      price: 9.99,
-      quantity: 1
-    }]
-});
+  dataLayer.push({
+    "event": "purchase",
+    "transaction_id": "T_12345",
+    "value": 25.42,
+    "tax": 4.90,
+    "shipping": 5.99,
+    "currency": "USD",
+    "coupon": "SUMMER_SALE",
+    "items": [
+      {
+        "item_id": "SKU_12345",
+        "item_name": "Stan and Friends Tee",
+        "affiliation": "Google Merchandise Store",
+        "coupon": "SUMMER_FUN",
+        "discount": 2.22,
+        "index": 0,
+        "item_brand": "Google",
+        "item_category": "Apparel",
+        "item_category2": "Adult",
+        "item_category3": "Shirts",
+        "item_category4": "Crew",
+        "item_category5": "Short sleeve",
+        "item_list_id": "related_products",
+        "item_list_name": "Related Products",
+        "item_variant": "green",
+        "location_id": "ChIJIQBpAG2ahYAR_6128GcTUEo",
+        "price": 9.99,
+        "quantity": 1
+      }
+    ]
+  });
 }
 
 function closePopup() {
   var popup = document.getElementById("popup-purchase");
   popup.style.display = "none"; // Hide the pop-up
 }
+
 <!-- End of purchase pop up -->
