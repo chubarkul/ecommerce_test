3f896576b6e31eafd6c64a4828e4ece4b9b23f95
chubarkul <58489131+chubarkul@users.noreply.github.com>
2023-06-20 17:43:55 +0600

    Update main.js

--- main.js
+++ main.js
@@ -108,28 +108,6 @@ function openPopup() {
       location_id: "ChIJIQBpAG2ahYAR_6128GcTUEo",
       price: 9.99,
       quantity: 1
-    },
-    {
-      item_id: "SKU_12346",
-      item_name: "Google Grey Women's Tee",
-      affiliation: "Google Merchandise Store",
-      coupon: "SUMMER_FUN",
-      discount: 3.33,
-      index: 1,
-      item_brand: "Google",
-      item_category: "Apparel",
-      item_category2: "Adult",
-      item_category3: "Shirts",
-      item_category4: "Crew",
-      item_category5: "Short sleeve",
-      item_list_id: "related_products",
-      item_list_name: "Related Products",
-      item_variant: "gray",
-      location_id: "ChIJIQBpAG2ahYAR_6128GcTUEo",
-      price: 20.99,
-      promotion_id: "P_12345",
-      promotion_name: "Summer Sale",
-      quantity: 1
     }]
 });
 }
