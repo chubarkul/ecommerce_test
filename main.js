7a2c4efac853f0703d8eb86e101083fa1b5f66b7
chubarkul <58489131+chubarkul@users.noreply.github.com>
2023-09-01 12:56:33 +0600

    Update main.js

--- main.js
+++ main.js
@@ -81,38 +81,18 @@ function openPopup() {
   popup.style.display = "block"; // Show the pop-up
                 
   // dataLayer.push for purchase
-  dataLayer.push({
-    "event": "purchase",
-    "transaction_id": "T_12345",
-    "value": 25.42,
-    "tax": 4.90,
-    "shipping": 5.99,
-    "currency": "USD",
-    "coupon": "SUMMER_SALE",
-    "items": [
-      {
-        "item_id": "SKU_12345",
-        "item_name": "Stan and Friends Tee",
-        "affiliation": "Google Merchandise Store",
-        "coupon": "SUMMER_FUN",
-        "discount": 2.22,
-        "index": 0,
-        "item_brand": "Google",
-        "item_category": "Apparel",
-        "item_category2": "Adult",
-        "item_category3": "Shirts",
-        "item_category4": "Crew",
-        "item_category5": "Short sleeve",
-        "item_list_id": "related_products",
-        "item_list_name": "Related Products",
-        "item_variant": "green",
-        "location_id": "ChIJIQBpAG2ahYAR_6128GcTUEo",
-        "price": 9.99,
-        "quantity": 1
-      }
-    ]
-  });
+  gtag("event", "view_item", {
+currency: ”KZT",
+items: [
+{
+item_id: "SKU_12345",
+item_name: "Детская кровать - тахта",
+item_category: "Кровати и матрасы",
+price: 99990.00,
+quantity: 1
 }
+]
+});
 
 function closePopup() {
   var popup = document.getElementById("popup-purchase");
